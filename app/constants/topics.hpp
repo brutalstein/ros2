31348b3f80fdcdf1dff85ff39b00e31b95bc91f0
Cenker Usta <cenkerusta@outlook.com>
2026-08-23 16:14:49 +0300

    Add PX4 offboard command topics

--- app/constants/topics.hpp
+++ app/constants/topics.hpp
@@ -16,4 +16,13 @@ namespace topics
 
     inline constexpr const char PX4_GNSS_SENSOR[] =
         "/fmu/out/vehicle_gps_position";
+
+    inline constexpr const char PX4_OFFBOARD_CONTROL_MODE[] =
+        "/fmu/in/offboard_control_mode";
+
+    inline constexpr const char PX4_TRAJECTORY_SETPOINT[] =
+        "/fmu/in/trajectory_setpoint";
+
+    inline constexpr const char PX4_VEHICLE_COMMAND[] =
+        "/fmu/in/vehicle_command";
 }
