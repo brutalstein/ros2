2e12eecc7c2bd5109791a1abd06cb3324e918f7f
Cenker Usta <cenkerusta@outlook.com>
2026-08-24 01:54:49 +0300

    Move PX4 publish logic into FlightPublisher

--- app/flight/publisher/publisher.cpp
+++ app/flight/publisher/publisher.cpp
@@ -24,3 +24,91 @@ FlightPublisher::FlightPublisher(rclcpp::Node &node)
             topics::PX4_VEHICLE_COMMAND,
             10);
 }
+
+void FlightPublisher::publish_offboard_heartbeat(
+    uint64_t timestamp)
+{
+    px4_msgs::msg::OffboardControlMode msg{};
+
+    msg.timestamp = timestamp;
+
+    msg.position = true;
+    msg.velocity = false;
+    msg.acceleration = false;
+    msg.attitude = false;
+    msg.body_rate = false;
+    msg.thrust_and_torque = false;
+    msg.direct_actuator = false;
+
+    offboard_control_mode_pub_->publish(msg);
+}
+
+void FlightPublisher::publish_hold_position(
+    uint64_t timestamp,
+    float x,
+    float y,
+    float z,
+    float yaw)
+{
+    const float nan =
+        std::numeric_limits<float>::quiet_NaN();
+
+    px4_msgs::msg::TrajectorySetpoint msg{};
+
+    msg.timestamp = timestamp;
+
+    msg.position = {
+        x,
+        y,
+        z
+    };
+
+    msg.velocity = {
+        nan,
+        nan,
+        nan
+    };
+
+    msg.acceleration = {
+        nan,
+        nan,
+        nan
+    };
+
+    msg.jerk = {
+        nan,
+        nan,
+        nan
+    };
+
+    msg.yaw = yaw;
+    msg.yawspeed = nan;
+
+    trajectory_setpoint_pub_->publish(msg);
+}
+
+void FlightPublisher::publish_vehicle_command(
+    uint64_t timestamp,
+    uint16_t command,
+    float param1,
+    float param2)
+{
+    px4_msgs::msg::VehicleCommand msg{};
+
+    msg.timestamp = timestamp;
+
+    msg.param1 = param1;
+    msg.param2 = param2;
+
+    msg.command = command;
+
+    msg.target_system = 1;
+    msg.target_component = 1;
+
+    msg.source_system = 1;
+    msg.source_component = 1;
+
+    msg.from_external = true;
+
+    vehicle_command_pub_->publish(msg);
+}
