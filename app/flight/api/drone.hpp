1799b6ef4e464e0188d42a70ecbade6c77837cc9
Cenker Usta <cenkerusta@outlook.com>
2026-08-24 15:46:36 +0300

    Keep Drone API focused on takeoff

--- app/flight/api/drone.hpp
+++ app/flight/api/drone.hpp
@@ -2,12 +2,13 @@
 
 #include "flight/state/flight_state.hpp"
 
-class Drone {
-  public:
+class Drone
+{
+public:
     explicit Drone(FlightState &state);
 
-    int takeoff(float altitude_m);
-    
-  private:
+    bool takeoff(float altitude_m);
+
+private:
     FlightState &state_;
 };
