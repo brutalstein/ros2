5a49274d2aa94f09ce3d1639bb460479e6442a09
Cenker Usta <cenkerusta@outlook.com>
2026-08-24 15:48:57 +0300

    Run only camera and flight nodes

--- app/runtime/node_registry.hpp
+++ app/runtime/node_registry.hpp
@@ -3,23 +3,17 @@
 #include <memory>
 #include <vector>
 
-#include "core/core.hpp"
-#include "state/state.hpp"
-#include "sensors/sensors.hpp"
 #include "camera/camera.hpp"
 #include "flight/flight.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 namespace drone_runtime
 {
-    std::vector<std::shared_ptr<rclcpp::Node>> make_nodes(){
-      std::vector<std::shared_ptr<rclcpp::Node>> nodes;
-
-      nodes.push_back(make_core_node());
-      nodes.push_back(make_state_node());
-      nodes.push_back(make_sensors_node());
-      nodes.push_back(make_camera_node());
-      nodes.push_back(make_flight_node());
-      return nodes;
-    }
+std::vector<std::shared_ptr<rclcpp::Node>> make_nodes()
+{
+    return {
+        make_camera_node(),
+        make_flight_node()
+    };
+}
 }
